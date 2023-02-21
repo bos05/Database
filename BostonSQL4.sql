2ee36d52c69dfb45f1a8561c23ee32fafa3e7f91
bos05 <111297813+bos05@users.noreply.github.com>
2023-02-21 08:21:41 -0700

    I have made the stubs for sql4

---
+++ BostonSQL4.sql
@@ -0,0 +1,76 @@
+--Boston Lignwall
+--Feb 21rst 2023
+--SQL 4
+
+--1
+SELECT
+    cost
+FROM 
+    course
+;
+
+--2
+SELECT
+    cost
+FROM    
+    course
+;
+
+--3
+SELECT
+    numeric_grade
+FROM
+    grade
+;
+
+--4
+SELECT
+    city,
+    state,
+    zipcode
+FROM
+    zipcode
+;
+
+--5
+SELECT
+    section_id
+FROM
+    enrollment
+;
+
+--6
+SELECT
+    student_id,
+    section_id
+FROM
+    grade
+;
+
+--7
+SELECT
+    student_id
+FROM
+    section
+;
+
+--8
+SELECT 
+    section_id
+FROM
+    grade
+;
+
+--9
+SELECT
+    employer
+FROM
+    student
+;
+
+--10
+SELECT
+    section_id
+FROM 
+    grade
+;
