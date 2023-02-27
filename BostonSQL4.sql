1406b1a9b81e65cf15a32dba674112f94b665b0b
bos05 <111297813+bos05@users.noreply.github.com>
2023-02-27 09:42:41 -0700

    i started with stubs on sql 5

--- BostonSQL4.sql
+++ BostonSQL4.sql
@@ -2,25 +2,34 @@
 --Feb 21rst 2023
 --SQL 4
 
---1
+--1 -- done
 SELECT
-    cost
+    ROUND(AVG(cost), 2)
 FROM 
     course
 ;
 
---2
-SELECT
-    cost
-FROM    
-    course
+--2 -- done
+SELECt
+    count(*) AS Febuary_Registrations
+FROM
+    enrollment
+WHERE
+    enroll_date like '%FEB-07%'
+--idk why but the number don't completly line up but this is the correct query i think
 ;
 
---3
+--3--done
 SELECT
-    numeric_grade
+    ROUND(AVG(numeric_grade), 1) AS Average,
+    MAX(numeric_grade) AS Highest,
+    MIN(numeric_grade) AS Lowest
 FROM
     grade
+WHERE 
+    grade_type_code like 'FI'
+    AND
+    section_id = '141'
 ;
 
 --4
---
+++ BostonSQL5.sql
@@ -0,0 +1,17 @@
+--Boston Lingwall
+--SQL 5
+--March 3rd 2023
+
+--1
+SELECT
+    first_name,
+    last_name,
+    phone
+FROM
+    student
+;
+
+--2
+SELECT
+    course_no, 
+;
