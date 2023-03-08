966be8653e05c5191898cdce00f953244dac5bd6
Boston <lingwallboston609@gmail.com>
2023-03-08 14:16:57 -0700

    ahhhhhhhhhhhhhhhhhhhhh none of this is working

--- BostonSQL5.sql
+++ BostonSQL5.sql
@@ -22,17 +22,77 @@ ORDER BY
     stu.first_name
 ;
 
---2
+--2--can not figure out how to have two descriptions
+SELECT
+    course.course_no,
+    course.description,
+    course.prerequisite,
+    course.description
+FROM
+    course course
+WHERE 
+    course_no BETWEEN 300 AND 399
+    AND
+    prerequisite IS NOT NULL
+GROUP BY
+    prerequisite
+
+;
+
+--3--gives to many things for some reason
 SELECT
     cou.course_no,
-    cou.description,
-    cou.prerequisite,
-    cour.description
+    cou.description, 
+    inst.first_name,
+    inst.last_name
 FROM
-    course cou
-JOIN
+    course cou, instructor inst
 
-ON 
-    cou.course_no = cour.course_no
+WHERE 
+    cou.course_no BETWEEN 100 AND 199
+    AND
+    inst.first_name = 'Charles'
+    AND
+    inst.last_name = 'Lowry'
+   
 ;
---3
+--4 --can not figure this out ahhhhhhhhh
+SELECT
+    gtw.grade_type_code AS grade,
+    gt.description,
+    COUNT(*) AS number_per_section
+FROM
+    grade_type gt
+JOIN 
+    grade_type_weight gtw
+ON
+    gt.grade_type_code = gtw.grade_type_code
+JOIN
+    section sec
+ON
+    gtw.section_id = sec.section_id
+WHERE
+    sec.course_no = 144
+GROUP BY 
+    gtw.grade_type_code
+;
+    
+--5 --done
+SELECT DISTINCT
+    stu.first_name,
+    stu.last_name,
+    TO_CHAR(enr.enroll_date, 'DAY MONTH dd, YYYY HH:MI') 
+FROM
+    student stu
+JOIN
+    enrollment enr
+ON
+    stu.student_id = enr.student_id
+WHERE 
+    TO_CHAR(enr.enroll_date, 'MM') LIKE '01'
+    AND
+    TO_CHAR(enr.enroll_date, 'YYYY') LIKE '2007'
+ORDER BY 
+    stu.last_name,
+    stu.first_name
+;  
