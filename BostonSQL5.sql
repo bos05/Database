3197a4ceecfccff685ed98587f665ca28351681f
bos05 <111297813+bos05@users.noreply.github.com>
2023-03-10 09:14:36 -0700

    finished sql 5

--- BostonSQL5.sql
+++ BostonSQL5.sql
@@ -22,59 +22,69 @@ ORDER BY
     stu.first_name
 ;
 
---2--can not figure out how to have two descriptions
-SELECT
-    course.course_no,
-    course.description,
-    course.prerequisite,
-    course.description
-FROM
-    course course
-WHERE 
-    course_no BETWEEN 300 AND 399
-    AND
-    prerequisite IS NOT NULL
-GROUP BY
-    prerequisite
-
-;
-
---3--gives to many things for some reason
+--2 done
 SELECT
     cou.course_no,
-    cou.description, 
-    inst.first_name,
-    inst.last_name
+    cou.description,
+    cou.prerequisite,
+    pre.description
 FROM
-    course cou, instructor inst
-
-WHERE 
-    cou.course_no BETWEEN 100 AND 199
-    AND
-    inst.first_name = 'Charles'
-    AND
-    inst.last_name = 'Lowry'
-   
-;
---4 --can not figure this out ahhhhhhhhh
-SELECT
-    gtw.grade_type_code AS grade,
-    gt.description,
-    COUNT(*) AS number_per_section
-FROM
-    grade_type gt
-JOIN 
-    grade_type_weight gtw
+    course cou
+JOIN
+    course pre
 ON
-    gt.grade_type_code = gtw.grade_type_code
+    pre.course_no = cou.prerequisite
+WHERE 
+    cou.course_no BETWEEN 300 AND 399
+    AND
+    cou.prerequisite IS NOT NULL
+ORDER BY
+    cou.prerequisite DESC
+
+;
+
+--3--done
+SELECT
+    cour.course_no,
+    cour.description
+FROM
+    course cour
 JOIN
     section sec
 ON
-    gtw.section_id = sec.section_id
+    sec.course_no = cour.course_no
+JOIN
+    instructor inst
+ON
+    sec.instructor_id = inst.instructor_id
 WHERE
-    sec.course_no = 144
-GROUP BY 
-    gtw.grade_type_code
+    cour.course_no BETWEEN 100 AND 199
+    AND
+    first_name LIKE 'Charles'
+    AND
+    last_name = 'Lowry'
+ORDER BY
+    course_no
+;
+
+--4 --done
+SELECT
+    grad.grade_type_code AS grade,
+    grad.description,
+    wei.number_per_section
+
+FROM
+    grade_type grad
+JOIN
+    grade_type_weight wei
+ON
+    grad.grade_type_code = wei.grade_type_code
+JOIN
+    section sec
+ON
+    wei.section_id = sec.section_id
+WHERE
+    course_no = 144
 ;
     
 --5 --done
@@ -96,3 +106,122 @@ ORDER BY
     stu.last_name,
     stu.first_name
 ;  
+
+--6 --done
+SELECT
+    (stu.last_name || ', ' || stu.first_name) AS full_name,
+    TO_CHAR(enr.enroll_date, 'MM/DD/YYYY'),
+    cour.course_no,
+    gtw.grade_type_code,
+    gra.numeric_grade
+FROM
+    student stu
+JOIN 
+    enrollment enr
+ON
+    stu.student_id = enr.student_id
+JOIN 
+    section sec
+ON
+    enr.section_id = sec.section_id
+JOIN
+    course cour
+ON 
+    sec.course_no = cour.course_no
+JOIN
+    grade_type_weight gtw
+ON
+    sec.section_id = gtw.section_id
+    
+JOIN
+    grade gra
+ON
+    gtw.section_id = gra.section_id
+    AND 
+    enr.student_id = gra.student_id
+    AND
+    gra.grade_type_code = gtw.grade_type_code
+WHERE
+    stu.last_name LIKE 'H%'
+ORDER BY
+    full_name
+;
+
+--7 --done
+SELECT
+    sec.section_id,
+    sec.section_no,
+    cour.description
+FROM
+    section sec
+JOIN 
+    course cour
+ON 
+    sec.course_no = cour.course_no
+WHERE
+    cour.prerequisite IS NULL
+;
+
+--8 --done
+SELECT DISTINCT
+    stu.first_name,
+    stu.last_name,
+    stu.phone
+FROM 
+    student stu
+JOIN
+    zipcode zip
+ON
+    stu.zip = zip.zip
+JOIN 
+    enrollment enr
+ON
+    stu.student_id = enr.section_id
+WHERE
+    zip.state LIKE 'NY'
+    AND 
+    zip.city LIKE 'Flushing'
+    AND
+    TO_DATE(enr.enroll_date, 'YYYY-MM-DD-HH-MI') < TO_Date('2007-02-02-10-20', 'YYYY-MM-DD-HH-MI')
+ORDER BY 
+    stu.last_name
+;
+--9 --done
+SELECT
+    cou.course_no,
+    cou.description
+FROM
+    course cou
+JOIN
+    section sec
+ON
+    sec.course_no = cou.course_no
+JOIN
+    grade_type_weight gtw
+ON
+    gtw.section_id = sec.section_id
+WHERE   
+    grade_type_code = 'PJ'
+;
+--10 --done
+SELECT
+    gra.section_id,
+    gra.grade_type_code AS code,
+    gra.numeric_grade
+FROM
+    grade gra
+JOIN
+    grade_type_weight gtw
+ON
+    gra.grade_type_code = gtw.grade_type_code
+    AND
+    gra.section_id = gtw.section_id
+WHERE
+    gtw.grade_type_code = 'QZ'
+    AND
+    drop_lowest = 'N'
+    AND
+    numeric_grade < 76
+ORDER BY
+    section_id
+;
