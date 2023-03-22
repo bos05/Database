36b4ff9efae25bb000f8458d151c513b6ae94adb
bos05 <111297813+bos05@users.noreply.github.com>
2023-03-22 09:50:00 -0600

    finished all of the stubs for sql 6

--- BostonSQL6.sql
+++ BostonSQL6.sql
@@ -44,4 +44,50 @@ FROM
 --5
 SELECT  
     sec.section_id
-    cou.course_no
+    cou.description,
+FROM
+    section sec,
+    course cou
+;
+
+--6
+SELECT  
+    zip.city,
+    zip.state,
+    zip.zip
+FROM
+    zpcode.zip 
+;
+
+--7
+SELECT
+    gra.grade_type_code
+FROM 
+    grade gra
+;
+
+--8
+SELECT
+    cou.description
+FROM    
+    course cou
+;
+
+--9
+SELECT
+    stu.student_id,
+    stu.first_name,
+    stu.last_name
+FROM    
+    student stu
+;
+
+--10
+SELECT  
+    stu.first_name, 
+    stu.last_name, 
+    gra.grade_type_code
+FROM
+    student stu, 
+    grade gra
+;
