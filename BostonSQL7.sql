b1497a7d1b2c305a486a73a51a21535d81675bd8
bos05 <111297813+bos05@users.noreply.github.com>
2023-04-10 09:50:04 -0600

    I some work on sql 7

--- BostonSQL7.sql
+++ BostonSQL7.sql
@@ -101,28 +101,83 @@ FROM
     course cou
 ;
 
---8
-SELECT
-    stu.first_name,
-    stu.last_name
-FROM
-    student stu 
-;
+--8-- not really working needs to be if only one of he students section id is has a start time of 10:30
+TO_CHAR(mycolumn, 'hh24:mi') = '10:30'
 
 --9
 SELECT
     stu.first_name,
-    stu.last_name
+    stu.last_name,
+    gra.numeric_grade
 FROM
     student stu
+JOIN
+    enrollment enr
+ON
+    stu.student_id = enr.student_id
+JOIN
+    grade gra
+ON
+    enr.student_id = gra.student_id
+    AND
+    enr.section_id = gra.section_id
+WHERE
+    gra.grade_type_code = 'FI'
+    AND
+    gra.section_id = 89
+    AND
+    gra.numeric_grade < 
+    (
+        SELECT 
+            AVG(gra.numeric_grade) AS avg_grade
+        FROM 
+            grade gra
+        WHERE
+            gra.grade_type_code = 'FI'
+            AND 
+            gra.section_id = 89
+    )
+ORDER BY
+    gra.numeric_grade DESC,
+    stu.last_name,
+    stu.first_name
 ;
 
---10
+--10 --done
 SELECT
     stu.first_name,
     stu.last_name,
     stu.phone
 FROM
     student stu
+JOIN
+    enrollment enr
+ON
+    stu.student_id = enr.student_id
+JOIN
+    section sec
+ON
+    enr.section_id = sec.section_id
+JOIN
+    course cou
+ON
+    sec.course_no = cou.course_no
+WHERE
+    cou.description LIKE 'Intro to SQL'
+    OR
+    cou.description LIKE 'Oracle Tools'
+    OR
+    cou.description LIKE 'PL/SQL Programming'
+    OR
+    cou.description LIKE 'Database Design'
+    OR
+    cou.description LIKE 'Database System Principles'
+    OR
+    cou.description LIKE 'DB Programming with Java'
+
+ORDER BY
+    stu.last_name
+    
+    
 ;
 
