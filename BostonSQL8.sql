f7570f89498d64f555d40617e8ac3ea7052fab3d
bos05 <111297813+bos05@users.noreply.github.com>
2023-04-20 09:53:04 -0600

    got a lot of the sql 8 probelms, sql  9 stubs are done

--- BostonSQL8.sql
+++ BostonSQL8.sql
@@ -92,30 +92,104 @@ ORDER BY
     total_capacity
 ;
 
---4
+--4 done
+WITH count1 AS
+(
 SELECT
     stu.student_id,
     stu.first_name,
     stu.last_name,
-    most_classes
-FROM 
+    count(enro.enroll_date) AS most_classes
+FROM
+    enrollment enro
+JOIN
     student stu
-;
-
---5
-SELECT
+ON
+    stu.student_id = enro.student_id
+GROUP BY
+    stu.student_id,
     stu.first_name,
     stu.last_name
+)
+SELECT
+    student_id,
+    first_name,
+    last_name,
+    most_classes
 FROM
-    student stu
+    count1
+WHERE
+    most_classes =
+    (
+        SELECT
+            MAX(most_classes)
+        FROM
+            count1
+    )
+ORDER BY
+    last_name,
+    first_name
 ;
 
---6 
-SELECT
+--5 done
+WITH count1 AS
+(
+    SELECT
+        stu.zip,
+        count(enro.enroll_date) AS num_students
+    FROM
+        student stu
+    JOIN
+        enrollment enro
+    ON
+        enro.student_id = stu.student_id
+    GROUP BY
+        stu.zip
+)
+SELECT DISTINCT
+    stu.first_name,
+    stu.last_name,
+    cou.num_students,
+    stu.zip
+FROM
+    student stu
+JOIN
+    count1 cou
+ON
+    stu.zip = cou.zip
+JOIN
+    enrollment enro
+ON
+    enro.student_id = stu.student_id
+WHERE
+    num_students =
+    (
+        SELECT
+            MAX(num_students)
+        FROM
+            count1
+    )
+ORDER BY
+    last_name,
+    first_name
+    
+;
+
+--6 done
+ SELECT
     cou.course_no,
-    cour.description
-FROM    
+    cou.description
+FROM
     course cou
+JOIN
+    section sec
+ON
+    sec.course_no = cou.course_no
+WHERE 
+    sec.instructor_id = 104
+ORDER BY
+    cou.description
+;
 
 --7
 SELECT
@@ -124,28 +198,96 @@ SELECT
 FROM
     student stu
 
---8
-SELECT
+--8 done
+SELECT DISTINCT
     stu.first_name,
     stu.last_name,
-    cou.description,
+    cou.Description,
     sec.section_id
-FROM    
-    student stu,
-    course cou,
+FROM
+    student stu
+JOIN
+    enrollment enro
+ON
+    enro.student_id = stu.student_id
+JOIN
     section sec
+ON
+    sec.section_id = enro.section_id
+JOIN
+    course cou
+ON
+    cou.course_no = sec.course_no
+JOIN
+    grade gra
+ON
+    gra.student_id = enro.student_id
+    AND
+    gra.section_id = enro.section_id
+WHERE
+    gra.grade_type_code LIKE 'MT'
+    AND
+    numeric_grade =
+    (
+    SELECT
+        MIN(numeric_grade)
+    FROM
+        grade
+    WHERE
+        grade_type_code LIKE 'MT'
+    )
+ORDER BY
+    stu.last_name,
+    stu.first_name,
+    cou.description
+;
 
---9
+--9 done
+WITH count1 AS
+(
 SELECT
+    section_id,
+    count(enroll_date) AS enrolled
+FROM
+    enrollment
+GROUP BY
+    section_id
+)
+SELECT
+    cou.enrolled,
     sec.section_id,
     sec.capacity
-FROM
+    
+FROM    
     section sec
+JOIN
+    count1 cou
+ON
+    cou.section_id = sec.section_id
+WHERE
+    cou.enrolled >= sec.capacity
+ORDER BY
+    cou.enrolled,
+    sec.section_id
 
---10
+;
+
+--10 done
 SELECT
     cou.course_no,
     cou.description,
-    cou.cost 
+    cou.cost
 FROM
-    courese cou
+    course cou
+WHERE
+    cou.cost =
+    (
+        SELECT
+            MIN(cou.cost)
+        FROM
+            course cou
+    )
+ORDER BY
+    cou.course_no,
+    cou.description
+;
--- BostonSQL9.sql
+++ BostonSQL9.sql
@@ -2,3 +2,85 @@
 --sql 9
 --APRIL 19TH
  
+
+--1
+SELECT
+    stu.student_id,
+    stu.last_name,
+    stu.first_name
+FROM
+    student stu
+;
+
+--2
+SELECT
+    ins.zip,
+    ins.first_name,
+    ins.last_name
+FROM
+    instructor ins
+;
+
+--3(combine with SET OPeration)
+SELECT
+    stu.first_name,
+    stu.last_name
+FROM
+    student stu
+;
+
+--4
+SELECT
+    sec.location
+FROM
+    section
+;
+
+--5
+SELECT
+    gra.grade_type_code,
+    gra.numeric_grade
+FROM
+    grade gra
+;
+
+--6
+SELECT
+    ins.first_name,
+    ins.last_name
+FROM
+    instructor ins
+;
+
+--7
+SELECT
+    cou.course_no,
+    cou.description
+FROM
+    course cou
+;
+
+--8
+SELECT
+    zip.zip,
+    zip.city
+FROM
+    zipcode zip
+;
+
+--9
+SELECT
+    cou.course_no,
+    cou.description
+FROM
+    course cou
+;
+
+--10
+SELECT
+    stu.student_id,
+    stu.first_name,
+    stu.last_name
+FROM
+    student stu
+;
